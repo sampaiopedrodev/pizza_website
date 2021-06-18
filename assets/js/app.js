579b0406d046a305a047cf4799a5ee82bb32ec15
Stilvio Pedro <stilviopedro15@gmail.com>
2021-06-18 23:02:40 +0100

    finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,13 @@
 // open and close menu mobile
-const btn_open_menu = document.querySelector('#btn-menu');
-const site_navegation = document.querySelector('.site-navegation');
+const btnOpenMenu = document.querySelector('#btn-menu');
+// const site_navegation = document.querySelector('.site-navegation');
 
-btn_open_menu.addEventListener('click', () =>{
-    if (!btn_open_menu.classList.contains('btnMenuActive')){
-        btn_open_menu.classList.add('btnMenuActive');
-        site_navegation.style.display = 'block';
+btnOpenMenu.addEventListener('click', () =>{
+    if (!btnOpenMenu.classList.contains('btnMenuActive')){
+        btnOpenMenu.classList.add('btnMenuActive');
     }
     else{
-        btn_open_menu.classList.remove('btnMenuActive');
-        site_navegation.style.display = 'none';
+        btnOpenMenu.classList.remove('btnMenuActive');
     }
         
 })
